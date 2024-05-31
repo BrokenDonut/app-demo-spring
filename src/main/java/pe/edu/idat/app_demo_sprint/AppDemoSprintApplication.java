06ab317cc4289370b6dc93f0657c27fc697f7ac7
ciberjean21 <pier_xd_21@hotmail.com>
2024-05-30 20:09:38 -0500

    nueva contribucion

---
+++ src/main/java/pe/edu/idat/app_demo_sprint/AppDemoSprintApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_demo_sprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDemoSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDemoSprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/app_demo_sprint/AppDemoSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_demo_sprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDemoSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
